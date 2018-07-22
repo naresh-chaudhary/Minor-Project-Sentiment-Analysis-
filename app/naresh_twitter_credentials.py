ca4b19f4a7714a40430dc2cb44615094fd6ab02d
Naresh Kumar <naresh10oct@gmail.com>
2018-07-23 01:35:38 +0530

    Delete naresh_twitter_credentials.py

--- app/naresh_twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-
-#consumer key, consumer secret, access token, access secret.
-ckey="MAwfDbRqDa54geK8dxy4Vj4s0"
-csecret="GzRZbkQxZYpfzy6Azhu5JhpfXuo10tFCpesVYFGed4AvLzh0DV"
-atoken="147847030-tkzBAv4d5LnLoxv6IwyRRmh1CBe6MgjLbtkUVoRB"
-asecret="Cbud9QKkT218O9dwWjNZqQXGPyg8PPeWYG1GompUiQlsT"
