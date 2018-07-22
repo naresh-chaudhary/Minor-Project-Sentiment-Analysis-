8a0de8bc0297c7356d0ea488a1a58ff2f5176d8a
Naresh Kumar <naresh10oct@gmail.com>
2018-07-23 01:35:13 +0530

    Delete krati_twitter_credentials.py

--- app/krati_twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-#consumer key, consumer secret, access token, access secret.
-ckey="8DCPWX1G3DusdeWIegmIO4Cnq"
-csecret="UtpgF3WszlSuc4wQckOZ7uL7Iqe0K9O2jMjaOMW8RyxpCLlrxy"
-atoken="717990809505628161-UYsrnnyEAJENVKmzZEk60eRaIU1Shms"
-asecret="97xxp7x42KtjTFwnuv7XuGunvIf8XXNcYwoLMqj3yKtm0"
